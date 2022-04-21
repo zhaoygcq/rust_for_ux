404f1c111fa806250ba840e138ff6a7e393da4a5
zhao kang <1060401583@qq.com>
2022-04-21 10:52:45 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("hello, {}", name));
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
